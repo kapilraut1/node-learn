0cc33d2947a225a1d1547ffd85d84d38b56a17dc
Kapil Raut <kapilraut026@gmail.com>
2025-01-19 10:32:48 +0545

    LEts

--- main.js
+++ main.js
@@ -4,5 +4,13 @@ console.log("Starting");
 
 fs.writeFile("love1.txt", "Hello this is kaps", ()=>{
   console.log("Done");
+  fs.readFile("love1.txt", (err, data)=>{
+    console.log(err, data);
+  })
 });
+
+fs.appendFile("love1.txt", " I love you", (e,d)=>{
+  console.log(d);
+})
 console.log("Ending");
+
