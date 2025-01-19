b0f4bf23838d274d34d3defe2fbf85b72e0b07c6
Kapil Raut <kapilraut026@gmail.com>
2025-01-20 00:29:16 +0545

    hola

---
+++ path.js
@@ -0,0 +1,8 @@
+import path from "path";
+console.log(path);
+
+let myPath= "D:\node1\path.js";
+console.log(path.dirname(myPath));
+console.log(path.extname(myPath));
+console.log(path.join("c://", "myname//hello"));
+
